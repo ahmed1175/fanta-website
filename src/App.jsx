c5c2e85f16a9ff87223173d3939c7221c9e5d173
ahmed1175 <a31213008@gmail.com>
2024-09-12 11:51:00 +0300

    qqa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 
 import Banner from "./component/Banner";
 import Blog from "./component/blog";
-import FAQ from "./component/FAQ"
 import Footer from "./component/Footer";
 import Hero from "./component/Hero";
 import Products from "./component/products";
@@ -16,7 +15,7 @@ function App() {
      <Banner />
      <TextBanner />
      <Blog />
-     <FAQ />
+  
      <Footer />
     </main>
   );
